a5f38105866a8b1d80273e38fecd2b551873f0ee
Prasanth T R <mailprasanthtr@gmail.com>
2017-04-16 12:00:40 +0530

    Master detail view in action

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,23 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Technologies';
 
-  techArr = [
-    "HTML",
-    "CSS",
-    "JavaScript",
-    "SASS",
-    "AngularJS"
-  ]
+  techArr = [{
+    "logo":"https://s3-us-west-2.amazonaws.com/svgporn.com/logos/html-5.svg",
+    "name":"HTML"
+  },{
+    "logo":"https://s3-us-west-2.amazonaws.com/svgporn.com/logos/css-3.svg",
+    "name":"CSS"
+  },{
+    "logo":"https://s3-us-west-2.amazonaws.com/svgporn.com/logos/javascript.svg",
+    "name":"JavaScript"
+  },{
+    "logo":"https://s3-us-west-2.amazonaws.com/svgporn.com/logos/angular-icon.svg",
+    "name":"Angular"
+  }];
+
+  tech = this.techArr[0];
+
+  onSelect (item) {
+    this.tech = item;
+  }
 }
